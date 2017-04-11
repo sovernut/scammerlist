2a203e9491685b43feb92b477dfecb87d87f373a
nut <orenomail4@gmail.com>
2017-04-11 10:42:40 +0700

    have index and title in index

---
+++ scammerlist/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Catalog(models.Model):
+    type_cat = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.type_cat
+
+class Person(models.Model):
+    catalog = models.ForeignKey(Catalog, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    mobile_number = models.IntegerField(default=0)
+    detail = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
+
---
+++ scammerlist/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render 
+from django.http import HttpResponse
+from .models import Person,Catalog 
+
+def index(request):
+    catalog = Catalog.objects.order_by('-type_cat') 
+    return render(request,"scammerlist/index.html",{'catalog':catalog}) 
