04f9942a04fd6c6b37264ca4b2a35afef7fae06c
nut <orenomail4@gmail.com>
2017-04-11 10:52:14 +0700

    add func test for title

--- scammerlist/views.py
+++ scammerlist/views.py
@@ -5,3 +5,8 @@ from .models import Person,Catalog
 def index(request):
     catalog = Catalog.objects.order_by('-type_cat') 
     return render(request,"scammerlist/index.html",{'catalog':catalog}) 
+
+def search(request):
+    query = request.GET['search_name']
+    print(query)
+    return index(request)
